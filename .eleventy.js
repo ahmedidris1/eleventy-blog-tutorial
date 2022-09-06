8942fc75e3088f8855b72a8405f7afbf471ccc31
ahmedidris1 <ahmeditmo1@gmail.com>
2022-09-06 20:00:51 +0300

    eleventified html template

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+const { DateTime } = require('luxon');
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.addPassthroughCopy('./src/style.css');
+
+  eleventyConfig.addFilter('postDate', (dateobj) => {
+    return DateTime.fromJSDate(dateobj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'public',
+    },
+  };
+};
